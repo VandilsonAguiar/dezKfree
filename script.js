1f6e338375bf38d655944623248bc480dbb6d600
Vandilson Rodrigues Aguiar <137460351+VandilsonAguiar@users.noreply.github.com>
2023-10-07 15:13:13 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const testForm = document.getElementById("test-form");
-
-    testForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Impede o envio do formulário real
-        window.location.href = "baiting.html"; // Redireciona para a página sobre baiting
-    });
-});
