2ee1b989aafb8d58b606e719301cfa22470663b1
Vandilson Rodrigues Aguiar <137460351+VandilsonAguiar@users.noreply.github.com>
2023-10-07 15:24:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     testForm.addEventListener("submit", function(event) {
         event.preventDefault(); // Impede o envio do formulário real
-        window.location.href = "https://vandilsonaguiar.github.io/dezKfree/baiting/baiting.html"; // Redireciona para a página sobre baiting
+        window.open("https://vandilsonaguiar.github.io/dezKfree/baiting/baiting.html", "_blank"); // Abre a página sobre baiting em uma nova aba/janela
     });
 });
